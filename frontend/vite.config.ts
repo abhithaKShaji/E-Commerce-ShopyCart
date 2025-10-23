ad7ef974b77eb0288b3ae9827cbd48458c36b72d
Abhitha K Shaji <abhithakshaji360@gmail.com>
2025-10-23 16:07:26 +0530

    change vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // make sure the build goes to 'dist'
+  },
+  base: '/', // ensures correct routing on Vercel
+});
