522e42e2d5fd902f4bdbf9edc2b6206590893a86
Abhitha K Shaji <abhithakshaji360@gmail.com>
2025-10-24 17:10:24 +0530

    order again

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db";
-import { getEnvVariable } from "./utils/helpers";
-import { addressRoute, authRoute, cartRoute, checkoutRoute,  orderRoute, productRoute } from "./routes";
+//import { getEnvVariable } from "./utils/helpers";
+import {
+  addressRoute,
+  authRoute,
+  cartRoute,
+  checkoutRoute,
+  orderRoute,
+  productRoute,
+} from "./routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,34 +17,44 @@ const PORT = process.env.PORT || 3000;
 // Connect Database
 connectDB();
 
-// Middlewares
-app.use(cors({
+// --------- MIDDLEWARES ----------
+app.use(
+  cors({
+    origin: [
+      "https://ecommerceshopycart.netlify.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors({
   origin: [
-    getEnvVariable('FRONT_END_URL'), "http://localhost:5173",
-  ], 
+    "https://ecommerceshopycart.netlify.app",
+    "http://localhost:5173",
+  ],
   credentials: true,
 }));
 
- 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Root 
-app.get("/", async (_req, res) => {
+// Root
+app.get("/", (_req, res) => {
   res.send("Hai there, API is running...");
 });
 
- //Routes
+// Routes
 app.use("/api/auth", authRoute);
-app.use('/api/products', productRoute)
+app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/checkout", checkoutRoute);
-app.use('/api/order', orderRoute);
-app.use('/api/address', addressRoute)
-
-
+app.use("/api/order", orderRoute);
+app.use("/api/address", addressRoute);
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(` Server running on port ${PORT}`);
 });
